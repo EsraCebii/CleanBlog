e1f9f1ed721e1b6447bc64cf2123cd2374c88538
Esra Cebi <esracebi.81@gmail.com>
2021-10-12 14:40:47 +0300

    ejs modulü

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+
+//Template Engine
+app.set('view engine', 'ejs');
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
 
 
 //MIDDLEWARES
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-});
 
 const port = 3000;
 app.listen(port, () => {
