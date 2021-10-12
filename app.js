296950803aae761f52b98e5625890144b172ac4a
Esra Cebi <esracebi.81@gmail.com>
2021-10-12 13:13:02 +0300

    statik dosyalar

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        name: 'Blog title',
-        description: 'Blog description'
-    }
-    res.send('blog')
-})
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
 
 const port = 3000;
 app.listen(port, () => {
